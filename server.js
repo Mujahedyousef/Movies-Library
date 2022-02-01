64d8f44d4a468ea70573f2792fa33d33e93fed83
Mujahedyousef <mujahedyousef97@gmail.com>
2022-02-01 22:34:41 +0200

    solving tasks 12

--- server.js
+++ server.js
@@ -4,43 +4,105 @@ const express=require('express');
 const app=express();
 const axios=require('axios')
 const dotenv = require('dotenv');
+const dMovie= require('./Movie Data/data.json');
+const { get } = require('express/lib/response');
 dotenv.config();
 const APIKEY = process.env.APIKEY;
-const dMovie= require('./Movie Data/data.json')
+const PORT=process.env.PORT;
 
-function Movie(title,poster_path,overview ){
+
+app.get("/",dataMovie)
+app.get("/favorite",(req,res)=>{
+    res.status(200).json("Welcome to Favorite Page")
+
+})
+app.get("/trending",TrendingHandler)
+
+app.get("/search",searchMovie)
+app.get("/moviePopular",pagePopularMovie)
+app.use(errorHandler);
+
+
+
+
+
+
+function MovieConstructor(id,title,release_date,poster_path,overview ){
+    this.Id=id;
     this.Title=title;
+    this.release_date=release_date;
     this.poster_path=poster_path;
     this.overview=overview;
 
 }
 
 
-app.get("/",(req,res)=>{
-    let movies = [];
-  moviesData.data.forEach((movie) => {
-    movie = new Movie(movie.title, movie.poster_path, movie.overview);
-    movies.push(movie);
+function dataMovie(req,res){
+  let dataAll = new MovieConstructor (dMovie.id,dMovie.title,dMovie.release_date,dMovie.poster_path,dMovie.overview);
+  return res.status(200).json(dataAll);
+};
+
+
+function searchMovie(req,res){
+let searchQuery  = (req.query.search);
+let searchArray=[];
+axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&query=${searchQuery}`).then(value=>{
+  value.data.results.forEach(shearchMovie=>{
+    let search =new MovieConstructor(shearchMovie.id,shearchMovie.title, shearchMovie.release_date,shearchMovie.poster_path, shearchMovie.overview);
+searchArray.push(search)
+  })
+  return res.status(200).json(searchArray);
+}).catch(error=>{
+  errorHandler(error, req,res);
+})
+}
+
+function TrendingHandler(req,res){
+let trendingMovies=[];
+axios
+.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${APIKEY}&language=en-US`).then(value=>{
+  value.data.results.forEach(trend=>{
+    let trendingMovie = new MovieConstructor (trend.id,trend.title,trend.release_date,trend.poster_path,trend.overview);            
+    trendingMovies.push(trendingMovie)
+  
+  })
+  return res.status(200).json(trendingMovies);
+
+}).catch(error=>{
+  errorHandler(error, req,res);
+})
+
+}
+
+function pagePopularMovie(req,res){
+  let popularMovie=[];
+  axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${APIKEY}&language=en-US&page=1
+  `).then(value =>{
+  value.data.results.forEach(value =>{
+    popularMovie.push(value);
+  });return res.status(200).json(popularMovie);
+  }).catch(error => {
+      errorHandler(error, req,res);
   });
-  return res.status(200).json(movies);
-});
-
-
-app.get("/favorite",(req,res)=>{
-    res.status(200).json("Welcome to Favorite Page")
-
-})
+ }
 
 
 
 
-app.use("*",(req,res)=>{
-    res.status(404).send("Not Found!")
-})
+ function errorHandler(error,req,res){
+   const err={
+    status : 500,
+    message : error
+}
+res.status(500).send(err);
+};
+
+ 
 
 
-app.listen(3000, ()=>{
-console.log('Server are working....')
+
+app.listen(PORT, ()=>{
+console.log(`Server are working ${PORT}....`)
 
 });
 
