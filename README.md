ed3529269337bf13598c92123c65c6d3ae1fad63
Mujahedyousef <mujahedyousef97@gmail.com>
2022-02-06 16:18:43 +0200

    update task 12

--- README.md
+++ README.md
@@ -10,11 +10,18 @@
 
 # Overview
 
-* Write "npm init -y" .
-
+* Write "npm init -y".
 * npm install express cors.
 * Import express library.
 * Const app=express();
-*  Write listen function.
+* Write listen function.
 * Can get any data. 
+* npm install axios dotenv.
+* get API from website.
+* make .env and write {API_KEY ,PORT}.
 
+# Project Features
+
+>1) the app allowing to the client to show any movie and show all information for it .
+
+>2) the app allowing to the client to show any movie and show all information for it .
--- server.js
+++ server.js
@@ -1,110 +1,129 @@
-"use strict"
-const express=require('express');
+//Use this to shown me any problem
+"use strict";
+//=======================All import and var====================================
 
+//=======1)imoprt module framework (express) from node_modules=================
+const express=require('express');
 const app=express();
-const axios=require('axios')
+
+//========================2)import and make variable ==========================
+const axios=require('axios');
 const dotenv = require('dotenv');
-const dMovie= require('./Movie Data/data.json');
-const { get } = require('express/lib/response');
-dotenv.config();
-const APIKEY = process.env.APIKEY;
+ dotenv.config();
+ const APIKEY=process.env.APIKEY;
 const PORT=process.env.PORT;
 
+//========================3)import data.json===================================
 
-app.get("/",dataMovie)
-app.get("/favorite",(req,res)=>{
-    res.status(200).json("Welcome to Favorite Page")
+const dMovie= require('./Movie Data/data.json');
+const { handle } = require('express/lib/application');
 
-})
-app.get("/trending",TrendingHandler)
-
-app.get("/search",searchMovie)
-app.get("/moviePopular",pagePopularMovie)
-app.use(errorHandler);
-
-
-
-
-
-
-function MovieConstructor(id,title,release_date,poster_path,overview ){
-    this.Id=id;
-    this.Title=title;
-    this.release_date=release_date;
-    this.poster_path=poster_path;
-    this.overview=overview;
-
-}
-
-
-function dataMovie(req,res){
-  let dataAll = new MovieConstructor (dMovie.id,dMovie.title,dMovie.release_date,dMovie.poster_path,dMovie.overview);
-  return res.status(200).json(dataAll);
+ //=======================make a constructor to movie==========================
+function dataMovie( id,title,release_date,poster_path,overview ){
+  this.id=id,
+this.title=title,
+ this.poster_path=release_date,
+this.poster_path=poster_path,
+this.overview=overview
 };
 
 
-function searchMovie(req,res){
-let searchQuery  = (req.query.search);
-let searchArray=[];
-axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&query=${searchQuery}`).then(value=>{
-  value.data.results.forEach(shearchMovie=>{
-    let search =new MovieConstructor(shearchMovie.id,shearchMovie.title, shearchMovie.release_date,shearchMovie.poster_path, shearchMovie.overview);
-searchArray.push(search)
+
+//=============================All method in express===========================
+//===================================1)get method==============================
+// use method especially to app called get(path,(function or callback(params)))
+
+app.get("/",(req,res)=>{
+    let movie=new Movie(dMovie.title,dMovie.poster_path,dMovie.overview);
+//The information is returned to the client and shown when sending the req to the path.
+    res.status(200).json(movie);
+});
+
+app.get("/favorite",(req,res)=>{
+    res.status(200).json("Welcome to Favorite Page")
+
+});
+
+app.get("/trending",getTrendingFunc);
+app.get("/search",getsearchFunc);
+app.get("/popular",pagePopularMovie);
+//===================================2)use method===============================
+
+//==============================include Handel Error and status=================
+
+//=========================1)status(500)Internal Server Error=================== 
+app.use(function (err,req,res){
+    console.log("Sorry, something went wrong");
+
+res.status(500).json("Sorry, something went wrong");
+});
+
+//=========================2)status(404)Not Found===============================
+app.use("*",handlError);
+
+//==============================Functions get from API==========================
+
+function getTrendingFunc (req,res){
+  let infTrendingMovies=[];
+  axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${APIKEY}&language=en-US'`).then(value=>{
+    value.data.results.forEach(trendingData=>{
+      let trendingMovies=new dataMovie (trendingData.id,trendingData.title,trendingData.release_date,trendingData.poster_path,trendingData.overview);
+      infTrendingMovies.push(trendingMovie) 
+    })
+    return res.status(200).json(infTrendingMovies);
+    
+  }).catch(err=>{
+   handlError(err,req,res)
   })
-  return res.status(200).json(searchArray);
-}).catch(error=>{
-  errorHandler(error, req,res);
-})
-}
+};
 
-function TrendingHandler(req,res){
-let trendingMovies=[];
-axios
-.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${APIKEY}&language=en-US`).then(value=>{
-  value.data.results.forEach(trend=>{
-    let trendingMovie = new MovieConstructor (trend.id,trend.title,trend.release_date,trend.poster_path,trend.overview);            
-    trendingMovies.push(trendingMovie)
-  
+function getsearchFunc(req,res){
+  let searchQuery=req.query.search;
+  let arraySearch=[];
+  axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&language=en-US&query=${searchQuery}page=1`).then(value=>{
+    value.data.results.forEach(searching=>{
+      let searchMovies=new dataMovie(searching.id,searching.title,searching.release_date,searching.poster_path,searching.overview);
+      arraySearch.push(searchMovies);
+    })
+return res.status(200).json(arraySearch);
+  }).catch(err=>{
+    handlError(err,req,res)
   })
-  return res.status(200).json(trendingMovies);
-
-}).catch(error=>{
-  errorHandler(error, req,res);
-})
-
-}
+};
 
 function pagePopularMovie(req,res){
   let popularMovie=[];
   axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${APIKEY}&language=en-US&page=1
   `).then(value =>{
-  value.data.results.forEach(value =>{
-    popularMovie.push(value);
-  });return res.status(200).json(popularMovie);
-  }).catch(error => {
-      errorHandler(error, req,res);
+  value.data.results.forEach(popular =>{
+    popularMovie.push(popular);});
+  return res.status(200).json(popularMovie);
+  }).catch(err => {
+      errorHandler(err, req,res);
   });
- }
+ };
 
 
+//===============function HandleError===========================================
 
-
- function errorHandler(error,req,res){
-   const err={
-    status : 500,
-    message : error
+function handlError (err,req,res){
+  console.log("Sorry, the page Not Found.");
+  res.status(404).json("Sorry, the page Not Found.");
+  
 }
-res.status(500).send(err);
-};
-
- 
 
 
 
+//=================3)Method listen for turn on the server an connection=========
 app.listen(PORT, ()=>{
-console.log(`Server are working ${PORT}....`)
+console.log(`Server are working on http://localhost:${PORT}`)
 
 });
+
+
+
+
+
 
 
 
