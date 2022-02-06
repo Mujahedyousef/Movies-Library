a7a8a0e0bae5fbb233fc4ac4027e50db6b80a2be
Mujahedyousef <mujahedyousef97@gmail.com>
2022-02-06 19:18:27 +0200

    update Task13

--- server.js
+++ server.js
@@ -1,140 +1,173 @@
-"use strict"
+//Use this to shown me any problem
+"use strict";
+//=======================All import and var====================================
+
+//=======1)imoprt module framework (express) from node_modules=================
 const express=require('express');
-
 const app=express();
-const axios=require('axios')
+
+//========================2)import and make variable ==========================
+const axios=require('axios');
 const dotenv = require('dotenv');
-const pg =require('pg')
-const dMovie= require('./Movie Data/data.json');
-const { get } = require('express/lib/response');
-const { Client } = require('pg/lib');
-const DATABASE_URL=process.env.DATABASE_URL;
-const client=new pg.Client(DATABASE_URL)
+
+const pg=require('pg'); //import pgPostgress
 dotenv.config();
-const APIKEY = process.env.APIKEY;
+const DATABASE_URL=process.env.DATABASE_URL;
+const client=new pg.Client(DATABASE_URL);//connected node.js to pg.
+const APIKEY=process.env.APIKEY;
 const PORT=process.env.PORT;
+app.use(express.json());
+//========================3)import data.json===================================
+
+const dMovie= require('./Movie Data/data.json');
 
 
-app.get("/",dataMovie)
+ //=======================make a constructor to movie==========================
+function dataMovie( id, title, release_date, poster_path, overview ){
+  this.id=id,
+this.title=title,
+ this.poster_path=release_date,
+this.poster_path=poster_path,
+this.overview=overview
+};
+
+
+
+//=============================All method in express===========================
+//===================================1)get method==============================
+// use method especially to app called get(path,(function or callback(params)))
+
+app.get("/",(req,res)=>{
+    let movie=new Movie(dMovie.title,dMovie.poster_path,dMovie.overview);
+//The information is returned to the client and shown when sending the req to the path.
+    res.status(200).json(movie);
+});
+
 app.get("/favorite",(req,res)=>{
     res.status(200).json("Welcome to Favorite Page")
 
-})
-app.get("/trending",TrendingHandler)
+});
 
-app.get("/search",searchMovie)
-app.get("/moviePopular",pagePopularMovie)
-app.post("/addfavariteMovie", addMovies);
-app.get("/getfavariteMovie", getAllFavariteMovies);
+app.get("/trending",getTrendingFunc);
+app.get("/search",getsearchFunc);
+app.get("/popular",pagePopularMovie);
+app.get("/movieInDatabase",getAllFavariteMovies);
+//===================================2)Post Method==============================
 
-app.use(errorHandler);
-
-
+app.post("/addMovie",addMovieFunc);
 
 
+//===================================3)use method===============================
 
+//==============================include Handel Error and status=================
 
-function MovieConstructor(id,title,release_date,poster_path,overview ){
-    this.Id=id;
-    this.Title=title;
-    this.release_date=release_date;
-    this.poster_path=poster_path;
-    this.overview=overview;
+//=========================1)status(500)Internal Server Error=================== 
+app.use(function (err,req,res){
+    console.log("Sorry, something went wrong");
 
-}
+res.status(500).json("Sorry, something went wrong");
+});
 
+//=========================2)status(404)Not Found===============================
+app.use("*",handlError);
 
-function dataMovie(req,res){
-  let dataAll = new MovieConstructor (dMovie.id,dMovie.title,dMovie.release_date,dMovie.poster_path,dMovie.overview);
-  return res.status(200).json(dataAll);
+//==============================Functions get from API==========================
+
+function getTrendingFunc (req,res){
+  let infTrendingMovies=[];
+  axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${APIKEY}&language=en-US'`).then(value=>{
+    value.data.results.forEach(trendingData=>{
+      let trendingMovies=new dataMovie (trendingData.id,trendingData.title,trendingData.release_date,trendingData.poster_path,trendingData.overview);
+      infTrendingMovies.push(trendingMovie) 
+    })
+    return res.status(200).json(infTrendingMovies);
+    
+  }).catch(err=>{
+   handlError(err,req,res)
+  })
 };
 
-
-function searchMovie(req,res){
-const searchQuery  = (req.query.search);
-const searchArray=[];
-axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&query=${searchQuery}`).then(value=>{
-  value.data.results.forEach(shearchMovie=>{
-    let search =new MovieConstructor(shearchMovie.id,shearchMovie.title, shearchMovie.release_date,shearchMovie.poster_path, shearchMovie.overview);
-searchArray.push(search)
+function getsearchFunc(req,res){
+  let searchQuery=req.query.search;
+  let arraySearch=[];
+  axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&language=en-US&query=${searchQuery}page=1`).then(value=>{
+    value.data.results.forEach(searching=>{
+      let searchMovies=new dataMovie(searching.id,searching.title,searching.release_date,searching.poster_path,searching.overview);
+      arraySearch.push(searchMovies);
+    })
+return res.status(200).json(arraySearch);
+  }).catch(err=>{
+    handlError(err,req,res)
   })
-  return res.status(200).json(searchArray);
-}).catch(error=>{
-  errorHandler(error, req,res);
-})
-}
-
-function TrendingHandler(req,res){
-const trendingMovies=[];
-axios
-.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${APIKEY}&language=en-US`).then(value=>{
-  value.data.results.forEach(trend=>{
-    let trendingMovie = new MovieConstructor (trend.id,trend.title,trend.release_date,trend.poster_path,trend.overview);            
-    trendingMovies.push(trendingMovie)
-  
-  })
-  return res.status(200).json(trendingMovies);
-
-}).catch(error=>{
-  errorHandler(error, req,res);
-})
-
-}
+};
 
 function pagePopularMovie(req,res){
-  const popularMovie=[];
+  let popularMovie=[];
   axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${APIKEY}&language=en-US&page=1
   `).then(value =>{
-  value.data.results.forEach(value =>{
-    popularMovie.push(value);
-  });return res.status(200).json(popularMovie);
-  }).catch(error => {
-      errorHandler(error, req,res);
+  value.data.results.forEach(popular =>{
+    popularMovie.push(popular);});
+  return res.status(200).json(popularMovie);
+  }).catch(err => {
+      errorHandler(err, req,res);
   });
- }
+ };
 
-
-
-
- function errorHandler(error,req,res){
-   const err={
-    status : 500,
-    message : error
-}
-res.status(500).send(err);
-};
-
- function addMovies(req,res){
-const addmovie=req.body;
-const sql=`INSERT INTO favariteMovie (title,release_date,poster_path,overview)VALUES($1,$2,$3,$4) RETURNING * ;`
-let values=[addmovie.title, addmovie.release_date, addmovie.poster_path, addmovie.overview ];
-client.query(sql, values).then((data) => {
-       
-  return res.status(201).json(data.rows);
-}).catch(error => {
-  errorHandler(error, req, res);
-})
-};
+ function getAllFavariteMovies(req,res){
+   const sql=`SELECT * FROM favariteMovie`;
+   client.query(sql).then(data=> {
+    return res.status(200).json(data.rows);
+  }).catch(err => {
+    handlError(err, req,res);
+  })
+  };
  
 
-function getAllFavariteMovies (req,res){
-const sql=`SELECT * FROM favariteMovie`;
-client.query(sql).then(data => {
-  return res.status(200).json(data.rows);
-}).catch(error => {
-  errorHandler(error, req,res);
+//==============================Function post===================================
+
+function addMovieFunc(req,res){
+let infAddMovie=req.body;
+const sql=`INSERT INTO favariteMovie (title, release_date, poster_path, overview) VALUS($1,$2,$3,$4) RETURNING *`;
+let infAdd=[infAddMovie.title,infAddMovie.release_date,infAddMovie.poster_path,infAddMovie.overview];
+client.query(sql,infAdd).then(data=>{
+  return res.status(201).json(data.rows);
+}).catch(err => {
+  handlError(err, req, res);
 })
-};
+}
 
 
+//===============function HandleError===========================================
+
+function handlError (err,req,res){
+  console.log("Sorry, the page Not Found.");
+  res.status(404).json("Sorry, the page Not Found.");
+  
+}
+
+
+
+//=================3)Method listen for turn on the server an connection=========
 
 client.connect().then(()=>{
 
   app.listen(PORT, ()=>{
-    console.log(`Server are working ${PORT}....`)
-    
-    });
+    console.log(`Server are working on http://localhost:${PORT}`)
+})
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
